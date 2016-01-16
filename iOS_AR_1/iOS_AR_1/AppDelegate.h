c7048316bffe343fa7deda7516aafdadc291e25b
mahakareem <mahakareem@gmail.com>
2016-01-16 10:31:18 +0300

    initial

---
+++ iOS_AR_1/iOS_AR_1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOS_AR_1
+//
+//  Created by maha k on 12/1/16.
+//  Copyright © 2016 maha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOS_AR_1/iOS_AR_1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOS_AR_1
+//
+//  Created by maha k on 12/1/16.
+//  Copyright © 2016 maha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
